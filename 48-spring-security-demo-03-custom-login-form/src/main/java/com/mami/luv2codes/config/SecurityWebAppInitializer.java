7cdf14dfeb7e9073d9fba5548aab5ea0fef55d92
Alexander Mami <algru81@gmail.com>
2021-11-09 17:51:59 +0300

    Rename project name

---
+++ 48-spring-security-demo-03-custom-login-form/src/main/java/com/mami/luv2codes/config/SecurityWebAppInitializer.java
@@ -0,0 +1,8 @@
+package com.mami.luv2codes.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+
+
+}
