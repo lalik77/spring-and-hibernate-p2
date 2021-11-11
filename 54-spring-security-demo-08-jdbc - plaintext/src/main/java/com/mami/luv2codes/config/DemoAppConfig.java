ab194d0f1e48fc29cc63ea2c23e39a7ce82a9abe
Alexander Mami <algru81@gmail.com>
2021-11-12 01:06:56 +0300

    Finish module 54

--- 54-spring-security-demo-08-jdbc - plaintext/src/main/java/com/mami/luv2codes/config/DemoAppConfig.java
+++ 54-spring-security-demo-08-jdbc - plaintext/src/main/java/com/mami/luv2codes/config/DemoAppConfig.java
@@ -1,16 +1,36 @@
 package com.mami.luv2codes.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.mami.luv2codes")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+
+    //set up variable to hold the properties
+
+    @Autowired
+   private  Environment environment;
+
+
+    //set up a logger for diagnostics
+
+    private Logger logger = Logger.getLogger(getClass().getName());
 
     //define a bean for ViewResolver
     @Bean
@@ -24,4 +44,69 @@ public class DemoAppConfig {
         return viewResolver;
 
     }
+
+    //define a bean for our security datasource
+
+    @Bean
+    public DataSource securityDataSource() {
+
+        //create connection pool
+
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        //set the jdbc driver class
+
+
+        try {
+            securityDataSource.setDriverClass(environment.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        //log the connection props
+        //for sanity's sake,log this info
+        //just to make sure we are REALLY reading data from properties file
+        logger.info(">>> jdbc.url=" + environment.getProperty("jdbc.url"));
+        logger.info(">>> jdbc.user=" + environment.getProperty("jdbc.user"));
+
+        //set database connection props
+
+        securityDataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
+        securityDataSource.setUser(environment.getProperty("jdbc.user"));
+        securityDataSource.setPassword(environment.getProperty("jdbc.password"));
+
+        //set connection pool props
+
+        securityDataSource.setInitialPoolSize(
+                getIntProperty("connection.pool.initialPoolSize"));
+
+        securityDataSource.setMinPoolSize(
+                getIntProperty("connection.pool.minPoolSize"));
+
+        securityDataSource.setMaxPoolSize(
+                getIntProperty("connection.pool.maxPoolSize"));
+
+        securityDataSource.setMaxIdleTime(
+                getIntProperty("connection.pool.maxIdleTime"));
+
+
+
+        return securityDataSource;
+    }
+
+
+    //need a helper method
+    //read environment property and convert it to int
+
+    private int getIntProperty(String propName){
+
+        String propVal = environment.getProperty(propName);
+        //now convert to int
+
+        int intPropVal = Integer.parseInt(propVal);
+
+        return intPropVal;
+
+    }
+
 }
